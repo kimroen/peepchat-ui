f3400f0d820fef7001702bbfb8c110c5663b9e30
Kim Røen <kim@kimroen.com>
2016-08-16 22:03:30 +0200

    Pass in model to component and define validations

--- app/routes/auth/login.js
+++ app/routes/auth/login.js
@@ -1,6 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return {
+      email: '',
+      password: ''
+    };
+  },
   actions: {
     doLogin() {
       alert('login attempted');
