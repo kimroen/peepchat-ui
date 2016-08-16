3199c51198751c74d09656924e7b732ffca72fde
Kim Røen <kim@kimroen.com>
2016-08-16 21:16:47 +0200

    Write a whole bunch of component code

--- app/components/x-input.js
+++ app/components/x-input.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
-export default Ember.Component.extend({
+const {
+  Component,
+  computed
+} = Ember;
+
+export default Component.extend({
+  classNames: ['input-field'],
+  type: 'text',
+
+  _errorMessages: computed('errors.[]', function() {
+    return (this.get('errors') || []).join(', ');
+  })
 });
