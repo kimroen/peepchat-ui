962405308a328b07ddfda7ae360e5ff6fb6392f3
Kim Røen <kim@kimroen.com>
2016-08-16 22:10:14 +0200

    Change the CREATE endpoint for users to go to register

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -3,5 +3,15 @@ import config from '../config/environment';
 
 export default JSONAPIAdapter.extend({
   host: config.DS.host,
-  namespace: config.DS.namespace
+  namespace: config.DS.namespace,
+
+  urlForCreateRecord(modelName/*, snapshot*/) {
+    switch(modelName) {
+      case 'user':
+      case 'users':
+        return this._super.apply(this, arguments).replace('users', 'register');
+      default:
+        return this._super(...arguments);
+    }
+  }
 });
