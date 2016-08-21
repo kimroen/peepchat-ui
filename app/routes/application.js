db25a54a466eec6190c9ca3ac26c5fab5e629ab7
Kim Røen <kim@kimroen.com>
2016-08-21 23:48:47 +0200

    Add toast message for logout

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,10 +1,18 @@
 import Ember from 'ember';
 import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
 
-export default Ember.Route.extend(ApplicationRouteMixin, {
+const { Route, inject } = Ember;
+
+export default Route.extend(ApplicationRouteMixin, {
+  flashMessages: inject.service(),
+
   actions : {
     logout() {
       this.get('session').invalidate();
+
+      // TODO: This is kind of pointless because the session
+      // invalidation above refreshes the browser before we get here
+      this.get('flashMessages').success('Logged out');
     }
   }
 });
